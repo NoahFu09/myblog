c9529bd6b7f68a6c517309ffd348a27288273e88
Noah Fu <pingchi996@gmail.com>
2023-04-24 23:06:51 +0800

    First Upload

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="auth">
+            <h1>Login</h1>
+            <form>
+                <input required type="text" placeholder="username" name="username" />
+                <input required type="password" placeholder="password" name="password" />
+                <button>Login</button>
+
+            </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>Hello this is Register!</div>
+  )
+}
+
+export default Register
