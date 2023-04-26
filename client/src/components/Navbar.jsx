27e28c8a839f6f386038874453f6f7c70ae37011
Noah Fu <pingchi996@gmail.com>
2023-04-26 20:29:45 +0800

    build login Page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../img/logo.png';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <img src={Logo} alt="Logo"></img>
+                </div>
+                <div className="links">
+                    <Link className="link" to="/?cat=life">
+                        <h6>吃喝玩樂</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=post">
+                        <h6>程式筆記</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=design">
+                        <h6>研究</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=food">
+                        <h6>食物</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=about">
+                        <h6>ABOUT ME</h6>
+                    </Link>
+                    <span>查斯特</span>
+                    <span>登出</span>
+                    <span className='write'>
+                        <Link className="link" to="/write">
+                            Write
+                        </Link>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Navbar
+export default Navbar;
