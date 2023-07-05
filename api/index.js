2a40c3bcd02b02d4bd211d73ae6238040cffb292
Noah Fu <pingchi996@gmail.com>
2023-07-05 23:57:20 +0800

    NickFu-安裝multer函式庫，尚未完成

--- api/index.js
+++ api/index.js
@@ -3,11 +3,18 @@ import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
 import cookieParser from 'cookie-parser';
-
+import multer from 'multer';
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/upload', upload.single('file'), function (req, res) {
+    res.status(200).json('Image has been uploaded.');
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -4,12 +4,18 @@ import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
     const [value, setValue] = useState('');
+    const [title, setTitle] = useState('');
+    const [file, setFile] = useState(null);
+    const [cat, setCat] = useState('');
 
-    console.log(value);
+    const handleClick = e => {
+        e.preventDefault();
+    };
+
     return (
         <div className="add">
             <div className="content">
-                <input type="text" placeholder="標題" />
+                <input type="text" placeholder="標題" onChange={e => setTitle(e.target.value)} />
                 <div className="editorContainer">
                     <ReactQuill className="editor" theme="snow" value={value} onChange={setValue} />
                 </div>
@@ -23,35 +29,35 @@ const Write = () => {
                     <span>
                         <b>可見度:</b> 公開
                     </span>
-                    <input style={{ display: 'none' }} type="file" name="" id="file" />
+                    <input style={{ display: 'none' }} type="file" name="" id="file" onChange={e => setFile(e.target.value[0])} />
                     <label className="file" htmlFor="file">
                         上傳照片
                     </label>
                     <div className="buttons">
                         <button>暫存</button>
-                        <button>上傳</button>
+                        <button onClick={handleClick}>更新</button>
                     </div>
                 </div>
                 <div className="item">
                     <h1>分類</h1>
                     <div className="cat">
-                        <input type="radio" name="cat" value="cinema" id="cinema" />
+                        <input type="radio" name="cat" value="cinema" id="cinema" onClick={e => setCat(e.target.value)} />
                         <label htmlFor="cinema">觀影心得</label>
                     </div>
                     <div className="cat">
-                        <input type="radio" name="cat" value="design" id="design" />
+                        <input type="radio" name="cat" value="design" id="design" onClick={e => setCat(e.target.value)} />
                         <label htmlFor="design">程式開發</label>
                     </div>
                     <div className="cat">
-                        <input type="radio" name="cat" value="music" id="music" />
+                        <input type="radio" name="cat" value="music" id="music" onClick={e => setCat(e.target.value)} />
                         <label htmlFor="music">音樂分享</label>
                     </div>
                     <div className="cat">
-                        <input type="radio" name="cat" value="food" id="food" />
+                        <input type="radio" name="cat" value="food" id="food" onClick={e => setCat(e.target.value)} />
                         <label htmlFor="food">美食分享</label>
                     </div>
                     <div className="cat">
-                        <input type="radio" name="cat" value="technology" id="technology" />
+                        <input type="radio" name="cat" value="technology" id="technology" onClick={e => setCat(e.target.value)} />
                         <label htmlFor="technology">科技探索</label>
                     </div>
                 </div>
