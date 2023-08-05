902abd49011130b0faf4c15584769dfb818667fe
Long-Tan-NickFu <pingchi996@gmail.com>
2023-08-06 00:23:55 +0800

    LongTan - 修改編輯文章 page，HTML & CSS

--- client/src/pages/Backend/Write.jsx
+++ client/src/pages/Backend/Write.jsx
@@ -8,9 +8,42 @@ const Write = () => {
 
     return (
         <div className="writebackend">
-            <input type="text" value={title} onChange={setTitle} placeholder="請輸入文章標題" />
+            <h2>編輯文章</h2>
+            <hr />
+            <div className="content">
+                <div className="box1">
+                    <span>文章標題：</span>
+                    <input type="text" value={title} size="30" onChange={e => setTitle(e.target.value)} />
+                </div>
 
-            <ReactQuill theme="snow" value={value} onChange={setValue} />
+                <div className="box2">
+                    <span>文章分類：</span>
+                    <div className="cat">
+                        <input type="radio" name="cat" id="design" value="design" />
+                        <label htmlFor="cat">程式設計</label>
+                    </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" id="food" value="food" />
+                        <label htmlFor="cat">美食分享</label>
+                    </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" id="technology" value="technology" />
+                        <label htmlFor="cat">科技探索</label>
+                    </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" id="music" value="music" />
+                        <label htmlFor="music">音樂分享</label>
+                    </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" id="cinema" value="cinema" />
+                        <label htmlFor="cinema">觀影心得</label>
+                    </div>
+                </div>
+
+                <div className="editorContainer">
+                    <ReactQuill theme="snow" value={value} onChange={setValue} />
+                </div>
+            </div>
         </div>
     );
 };
